62d869b6a5ca18c55b616b73bc5092171b3b0d16
Adrian Grimm <usmcamgrimm@gmail.com>
2022-04-25 06:12:42 -0400

    Lightsaber codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'blue': '#2e67f8', // rgb(46, 103, 248)
+      'red': '#eb212e', // rgb(235, 33, 46)
+      'green': '#2ff924', // rgb(47, 249, 36)
+    },
   },
   plugins: [],
 }
